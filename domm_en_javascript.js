789c03858325868924ae8d0ef25a6f216f16c147
Wili Delgado <wilsondelgadomoreno@gmail.com>
2025-11-09 18:06:14 -0500

    Aprendiendo Doom

---
+++ domm_en_javascript.js
@@ -0,0 +1,9 @@
+// que son los nodos todo lo que se conforma como etiquetas son nodos es la estrcutura del arbol 
+//hay muchos tipos de nodos 
+//document:es todo lo que abaraca el nodo
+//Element : nodo que se definen alas etiquetas html 
+//text: lso atributos son hijos en modo texto
+//informacion asociada a otro elemento
+//Metodo de selecion de elementos
+document.getElementById(); // por id se seleciona
+document.getElementByTagName();
