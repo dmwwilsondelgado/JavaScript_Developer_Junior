a0764ba4bac22dd770f4418740a0c2156f2857ef
Wili Delgado <wilsondelgadomoreno@gmail.com>
2025-11-09 15:32:38 -0500

    programicon instaciamos el objeto del controlador

--- class_ejemplos.js
+++ class_ejemplos.js
@@ -1,8 +1,19 @@
-class animal {
-    constructor(especie,edad,color){
+class animal { //clase
+    constructor(especie,edad,color){ // parametros que cosa tiene un animal
+        //objeto = this. va tener una pripedad atributo 
         this.esp = especie;
         this.edad = edad;
         this.color = color;
+        this.info = `Soy ${this.esp},tengo${this.edad},años y soy ${this.color}`
     }
 }
-let perro = new animal()
+// variable  declarada a la clase y vamos a instanciarlo 
+let perro = new animal( "frez buder", "5","blanco");
+
+//document.write(perro);
+console.log(perro);
+
+//como hacemos para aceder 
+document.writeln(perro.info);
+
+
