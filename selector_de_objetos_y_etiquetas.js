0a9b4fa12c31cda9cf70fdb25f446c72df5770d2
Wili Delgado <wilsondelgadomoreno@gmail.com>
2025-11-10 12:26:02 -0500

    practique lo visto ayer construtor edn programacion orientada a objetos

--- selector_de_objetos_y_etiquetas.js
+++ selector_de_objetos_y_etiquetas.js
@@ -10,3 +10,20 @@ console.log(rangoEtario.getAttribute("type"));// obtenemos el resultado del nodo
 //console.log(rangoEtario.setAttribute("type"));
 //console.log(rangoEtario.removeAttribute("type")); // para remover el atrubito
 
+
+
+/// Clase practica de lo visto ayer 
+
+class carros {
+    constructor(color,tipo,marca,velocidadFinal){
+        this.color = color;
+        this.tipo = tipo;
+        this.marca = marca;
+        this.velocidad = velocidadFinal;
+        this.info = `El auto es de color ${this.color}. de la marca ${this.marca}`;
+    }
+}
+
+let carro = new carros("rojo","pequeño","mazda","210km");
+console.log(carro.info);
+// el contrutor en javascript siempre se lalmara construtor si no no lo detecta 
