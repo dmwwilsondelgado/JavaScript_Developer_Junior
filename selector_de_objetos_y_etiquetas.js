6f7f0d2b852e352418f6f7c265fa5adc2c63ad68
Wili Delgado <wilsondelgadomoreno@gmail.com>
2025-11-10 12:12:36 -0500

    empiezamos de nuevo a repasar

--- selector_de_objetos_y_etiquetas.js
+++ selector_de_objetos_y_etiquetas.js
@@ -8,4 +8,5 @@ rangoEtario.setAttribute("type","wilson");
 
 console.log(rangoEtario.getAttribute("type"));// obtenemos el resultado del nodo
 //console.log(rangoEtario.setAttribute("type"));
-console.log(rangoEtario.removeAttribute("type")); // para remover el atrubito
+//console.log(rangoEtario.removeAttribute("type")); // para remover el atrubito
+
