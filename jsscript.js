854d8d951413a465e202d8e1d13d8bc04255dc35
Wili Delgado <wilsondelgadomoreno@gmail.com>
2025-11-11 11:02:37 -0500

    Repasando constructor

---
+++ jsscript.js
@@ -0,0 +1,15 @@
+class motos{
+    constructor(marca,referencia,color){
+        this.marca = marca;
+        this.referencia = referencia;
+        this.color = color;
+        this.info = `la moto es ${this.referencia} 
+        de la marca ${this.marca} 
+        y su color original es ${this.color}`
+    }
+}
+let moto001 =  new motos("yamaha","mt15","blanco narajana");
+console.log(moto001.info);
+
+
+
